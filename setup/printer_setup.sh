271e1b1d6c3296e88cd510ea7b6f01d6fe3cbb8b
james <jimmy@cyberserv.co.za>
2020-05-07 20:00:07 +0200

    Initial
    Signed-off-by: james <jimmy@cyberserv.co.za>

--- setup/printer_setup.sh
+++ setup/printer_setup.sh
@@ -2,7 +2,6 @@
 #!/bin/bash
 
 cd ~
-
 sudo apt -y  update
 sudo apt -y  full-upgrade
 sudo apt-get -y install nginx
@@ -28,6 +27,10 @@ tar -xvzf "ARGOX_RPi_Printer_Driver-V1.5.0(armhf).tar.gz"
 cd 'ARGOX_RPi_Printer_Driver-V1.5.0(armhf)'
 sudo ./install
 
+cd ~
+sudo git clone https://github.com/James3539/flaskPrint.git
+
+sudo chown www-data /home/pi/flaskPrint
 
 
 
