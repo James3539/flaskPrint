ac8e9360aa799ba77e36b994e273155ddc22158e
james <jimmy@cyberserv.co.za>
2020-05-08 01:27:17 +0200

    Initial
    Signed-off-by: james <jimmy@cyberserv.co.za>

--- setup/printer_setup.sh
+++ setup/printer_setup.sh
@@ -1,25 +1,32 @@
 
-
 #!/bin/bash
-sudo su
 cd ~
 sudo apt -y  update
 #sudo apt -y  full-upgrade
 sudo apt-get -y install nginx
 sudo apt-get -y install python3-pip
 sudo apt-get -y install wkhtmltopdf
-
-sudo apt-get -y install cups
-sudo apt-get -y install libcups2-dev
-
 sudo apt-get -y install wget git-core
+sudo apt-get install build-essential python-dev
 
 sudo pip3 install flask uwsgi
 sudo pip3 install flask-api
 sudo pip3 install Pillow
 sudo pip3 install imgkit
-sudo pip3 install pycups
 sudo pip3 install beautifulsoup4
+
+sudo git clone https://github.com/James3539/flaskPrint.git
+sudo chown www-data /home/pi/flaskPrint
+sudo cp /home/pi/flaskPrint/setup/rc.local  /etc/rc.local
+sudo rm /etc/nginx/sites-enabled/default
+sudo cp /home/pi/flaskPrint/setup/flaskPrint_proxy   /etc/nginx/sites-available/flaskPrint_proxy
+sudo ln -s /etc/nginx/sites-available/flaskPrint_proxy /etc/nginx/sites-enabled
+
+sudo service nginx restart
+
+sudo apt-get -y install cups
+sudo apt-get -y install libcups2-dev
+sudo pip3 install pycups
 
 cd /home/pi
 mkdir downloads
@@ -29,14 +36,7 @@ tar -xvzf "ARGOX_RPi_Printer_Driver-V1.5.0(armhf).tar.gz"
 cd 'ARGOX_RPi_Printer_Driver-V1.5.0(armhf)'
 sudo ./install
 
-cd ~
-sudo chown www-data /home/pi/flaskPrint
-sudo cp /home/pi/flaskPrint/setup/rc.local  /etc/rc.local
-sudo rm /etc/nginx/sites-enabled/default
-sudo cp /home/pi/flaskPrint/setup/flaskPrint_proxy   /etc/nginx/sites-available/flaskPrint_proxy
-sudo ln -s /etc/nginx/sites-available/flaskPrint_proxy /etc/nginx/sites-enabled
 
-sudo service nginx restart
 
 
 
